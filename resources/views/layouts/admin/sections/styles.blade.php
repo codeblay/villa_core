b7cedb702c5c916f7da32dbb7a84e1a1138aa222
Lalu Ahdiyat <ahdiyatlalu@gmail.com>
2024-03-21 03:43:09 +0700

    select2 glitch overflow

--- resources/views/layouts/admin/sections/styles.blade.php
+++ resources/views/layouts/admin/sections/styles.blade.php
@@ -14,7 +14,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css">
 
 <style>
-    .select2-container {
+    .select2 {
         width: 100% !important;
     }
 

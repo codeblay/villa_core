20f442ec0a94782e0635540216f0f687a891a7f8
Lalu Ahdiyat <ahdiyatlalu@gmail.com>
2024-03-22 02:42:47 +0700

    init log midtrans

--- config/logging.php
+++ config/logging.php
@@ -133,6 +133,15 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
             'replace_placeholders' => true,
+
+        ],
+        
+        'midtrans' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/midtrans/midtrans.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+            'replace_placeholders' => true,
         ],
     ],
 

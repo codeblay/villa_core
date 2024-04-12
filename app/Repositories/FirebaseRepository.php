f813676c1d3da05c06a4f8ddacb9de41400a5993
Lalu Ahdiyat <ahdiyatlalu@gmail.com>
2024-04-12 22:28:37 +0700

    fcm update

--- app/Repositories/FirebaseRepository.php
+++ app/Repositories/FirebaseRepository.php
@@ -2,26 +2,34 @@
 
 namespace App\Repositories;
 
+use Google_Client;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 
 final class FirebaseRepository
 {
     public string $url;
-    public string $server_key;
     public string $project_id;
 
     public function __construct()
     {
         $this->url          = config('firebase.url');
-        $this->server_key   = config('firebase.server_key');
         $this->project_id   = config('firebase.project_id');
+    }
+
+    function getToken() : string {
+        $config = json_decode(file_get_contents("firebase.json"), true);
+        $c = new Google_Client();
+        $c->setAuthConfig($config);
+        $c->setScopes("https://www.googleapis.com/auth/firebase.messaging");
+
+        return $c->fetchAccessTokenWithAssertion()["access_token"];
     }
 
     function send(string $token, string $title, string $body): Response
     {
         $request = Http::acceptJson()
-            ->withToken($this->server_key)
+            ->withToken($this->getToken())
             ->contentType('application/json')
             ->post("{$this->url}/v1/projects/{$this->project_id}/messages:send", [
                 "message" => [
--- config/firebase.php
+++ config/firebase.php
@@ -2,6 +2,5 @@
 
 return [
     'url'           => env("FIREBASE_URL", ""),
-    'server_key'    => env("FIREBASE_SERVER_KEY", ""),
     'project_id'    => env("FIREBASE_PROJECT_ID", ""),
 ];
